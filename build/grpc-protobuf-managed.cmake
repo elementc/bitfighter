e19dbedaedef4eb65ec75dfe800347d560fae80d
Casey Doran <caseyd.doran@gmail.com>
2020-07-18 23:49:37 -0700

    will need the new cmake package of course

---
+++ build/grpc-protobuf-managed.cmake
@@ -0,0 +1,70 @@
+include(FetchContent)
+FetchContent_Declare(
+  gRPC
+  GIT_REPOSITORY https://github.com/grpc/grpc
+  GIT_TAG        v1.28.0
+  )
+set(FETCHCONTENT_QUIET OFF)
+FetchContent_MakeAvailable(gRPC)
+
+# Since FetchContent uses add_subdirectory under the hood, we can use
+# the grpc and protobuf targets directly from this build.
+set(_PROTOBUF_LIBPROTOBUF libprotobuf)
+set(PROTOBUF_LIBRARY libprotobuf)
+set(_REFLECTION grpc++_reflection)
+set(_PROTOBUF_PROTOC $<TARGET_FILE:protoc>)
+set(_GRPC_GRPCPP grpc++)
+set(GRPC_LIBRARY grpc++)
+if(CMAKE_CROSSCOMPILING)
+  find_program(_GRPC_CPP_PLUGIN_EXECUTABLE grpc_cpp_plugin)
+else()
+  set(_GRPC_CPP_PLUGIN_EXECUTABLE $<TARGET_FILE:grpc_cpp_plugin>)
+endif()
+
+# A utility function that:
+# 1) Adds a build system rule for generating C++ headers and sources from the protobuf file <protofile_name>
+# 2) Appends output filenames to the following lists:
+#   - generated_pb_srcs: list of all .pb.cc files.
+#   - generated_grpc_srcs: list of all .grpc.pb.cc files.
+#   - generated_pb_hdrs: list of all .pb.h files.
+#   - generated_grpc_hdrs: list of all .grpc.pb.h files.
+#
+# Make sure to add the srcs lists to your executable or library so that your protobuf cc files get built,
+# and to add ${CMAKE_CURRENT_BINARY_DIR}/pb_headers/ to your include path, so that your headers are findable.
+#
+function(generate_cpp_from_protofile protofile_name)
+  # Slice and dice the input protofile name.
+  get_filename_component(absolute_protofile_name ${protofile_name} ABSOLUTE)
+  get_filename_component(base_protofile_name ${protofile_name} NAME_WE)
+  get_filename_component(protofile_path "${absolute_protofile_name}" PATH)
+
+  # Assemble generated protoc output file names.
+  set(proto_src "${CMAKE_CURRENT_BINARY_DIR}/${base_protofile_name}.pb.cc")
+  set(proto_hdr "${CMAKE_CURRENT_BINARY_DIR}/pb_headers/${base_protofile_name}.pb.h")
+  set(grpc_src "${CMAKE_CURRENT_BINARY_DIR}/${base_protofile_name}.grpc.pb.cc")
+  set(grpc_hdr "${CMAKE_CURRENT_BINARY_DIR}/pb_headers/${base_protofile_name}.grpc.pb.h")
+
+  # Abort if a generated file name matches one we've already written a rule for-
+  # they must be uniquely named or the protoc step will overwrite the one that runs first.
+  if (${proto_src} IN_LIST generated_pb_srcs)
+    message(FATAL_ERROR "Tried to make a rule for ${proto_src}, but one has already been generated. Protofile names must be globally unique.")
+  endif()
+
+  # Add a build rule for protoc to write out the files determined above.
+  add_custom_command(
+        OUTPUT "${proto_src}" "${proto_hdr}" "${grpc_src}" "${grpc_hdr}"
+        COMMAND ${_PROTOBUF_PROTOC}
+        ARGS --grpc_out "${CMAKE_CURRENT_BINARY_DIR}"
+          --cpp_out "${CMAKE_CURRENT_BINARY_DIR}"
+          -I "${protofile_path}"
+          --plugin=protoc-gen-grpc="${_GRPC_CPP_PLUGIN_EXECUTABLE}"
+          "${absolute_protofile_name}"
+        DEPENDS "${protofile_path}")
+
+  # Add the generated file names to the list of generated proto files, so that they can be included in builds.
+  set(generated_pb_srcs ${proto_src} ${generated_pb_srcs} PARENT_SCOPE)
+  set(generated_grpc_srcs ${grpc_src} ${generated_grpc_srcs} PARENT_SCOPE)
+  set(generated_pb_hdrs ${proto_hdr} ${generated_pb_hdrs} PARENT_SCOPE)
+  set(generated_grpc_hdrs ${proto_src} ${generated_grpc_hdrs} PARENT_SCOPE)
+
+endfunction()
